2d02017b679bba6abd51bf54c48963d46cf11db5
Kittipitch_1009 <kittipitch.mold@mail.kmutt.ac.th>
2023-02-07 17:17:13 +0700

    Create Repo and working on Homework No1 and No3

---
+++ No1/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program {
+
+    private static double Cal(double x) {
+
+        if ( x*x <= 4) {
+            return 2;
+        }
+        else {
+            return 1 + Cal(x - ((4 * Math.Abs(x)) / x));
+        }
+    }
+
+    public static void Main(String[] args) {
+
+        Console.Clear();
+
+        Console.Write("Input value of x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("f({0}) = {1}", x, Cal(x));
+    }
+}
---
+++ No3/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program {
+
+    private static double Cal(double x) {
+
+        if ((x > -1) && (x <= 0)) {
+            return Math.Sqrt(1 - (x*x));
+        }
+        else if ((x > 0) && (x <= 1)) {
+            return -Math.Sqrt(1 - (x*x));
+        }
+        else if (x <= -1) {
+            return Cal(x + 2);
+        }
+        else {
+            return Cal(x - 2);
+        }
+    }
+
+    public static void Main(String[] args) {
+
+        Console.Clear();
+
+        Console.Write("Input value of x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("f({0}) = {1}", x, Cal(x));
+    }
+}
